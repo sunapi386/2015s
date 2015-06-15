a54c5dff777e2360a736a9a04e57f50a2bbb57ca
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-15 15:42:36 -0400

    submit

--- cs486/a2/fraud.rb
+++ cs486/a2/fraud.rb
@@ -45,36 +45,45 @@ ordering = ["Trav", "FP", "IP", "OC", "CRP"]
 factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
 
 # Question 2b Prior
-puts "\r\n====== Question 2b prior: Jon Snow Credit Co. "
+puts "\r\n====== Question 2b prior: Jon Snow's Credit Company"
 evidences = {}
-result = inference(factors, queryVars, ordering, evidences)
-print_summary(result, evidences)
+result_2b_1 = inference(factors, queryVars, ordering, evidences)
+print_summary(result_2b_1, evidences)
 
 # Question 2b Posterior
 puts "\r\n====== Question 2b posterior: Nerd makes foreign purchase offline"
 evidences = {"FP" => 1, "IP" => 0, "CRP" => 1}
-result = inference(factors, queryVars, ordering, evidences)
-print_summary(result, evidences)
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
+result_2b_2 = inference(factors, queryVars, ordering, evidences)
+print_summary(result_2b_2, evidences)
 
 # Question 2c
-puts "\r\n====== Question 2c: Jon Snow Co. verfies, his client is out tripping"
+puts "\r\n====== Question 2c: Jon Snow Co. verfies his client is tripping out"
 evidences = {"FP" => 1, "IP" => 0, "CRP" => 1, "Trav" => 1}
-result = inference(factors, queryVars, ordering, evidences)
-print_summary(result, evidences)
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
+result_2c = inference(factors, queryVars, ordering, evidences)
+print_summary(result_2c, evidences)
+diff = print_change(result_2b_2, result_2c)
+puts "After calling we are #{diff * 100} % more confident"
 
 # Question 2d
-puts "\r\n====== Question 2d: Credit Card Company has a traitor"
+puts "\r\n====== Question 2d: Jon Snow's traitor"
 evidences = {"IP" => 1}
-result = inference(factors, queryVars, ordering, evidences)
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
+result_2d_1 = inference(factors, queryVars, ordering, evidences)
 puts "Before infiltration:"
-print_summary(result, evidences)
+print_summary(result_2d_1, evidences)
 evidences = {"Trav" => 0, "IP" => 1, "CRP" => 1, "FP" => 0}
-result = inference(factors, queryVars, ordering, evidences)
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
+result_2d_2 = inference(factors, queryVars, ordering, evidences)
 puts "After infiltration:"
-print_summary(result, evidences)
+print_summary(result_2d_2, evidences)
+diff = print_change(result_2d_2, result_2d_1)
+puts "After preparing to steal, chance of getting caught #{diff * 100}%"
 
 # Question 3b
 evidences = {"FP" => 1, "IP" => 0, "CRP" => 1}
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
 fraud = inference(factors, queryVars, ordering, evidences)
 result = sumout(multiply(fraud, Utility), "Fraud")
 puts "\r\n====== Question 3b: Block nerd's $1000 foreign purchase made offline?"
@@ -82,6 +91,7 @@ print_summary(result, evidences)
 
 # Question 3c
 evidences = {"FP" => 1, "IP" => 0, "CRP" => 1, "Trav" => 1}
+factors = [Fraud.clone, Trav.clone, FP.clone, IP.clone, CRP.clone, OC.clone]
 fraud = inference(factors, queryVars, ordering, evidences)
 result = sumout(multiply(fraud, Utility), "Fraud")
 puts "\r\n====== Question 3c: Block same transaction knowing he's travelling?"
