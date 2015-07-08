e0d21afcfe3c762081864a6e4d26db376cf32c21
Jason Sun <jason.sun@uwaterloo.cam>
2015-07-07 21:08:48 -0400

    naive_bayes.m and naive_bayes_net.m

--- cs486/a3/naive_bayes.m
+++ cs486/a3/naive_bayes.m
@@ -1,41 +1,33 @@
 loadScript
-
-% split training set into two documents
-doc1 = [];
-doc2 = [];
-for i = 1:length(trainLabel)
-   if trainLabel(i) == 1
-       doc1 = [doc1; trainDataSparse(i,:)];
-   else
-       doc2 = [doc2; trainDataSparse(i,:)];
-   end
-end
-
-% calc Pr(word|label1) and Pr(word|label2)
-word_likelihood1 = zeros(length(trainDataSparse(1,:)),1);
-word_likelihood2 = zeros(length(trainDataSparse(1,:)),1);
-
-for word = 1:length(trainDataSparse(1,:))
-   count = 1;
-   for i = 1:size(doc1, 1)
-      count = count + doc1(i,word); 
-   end
-   word_likelihood1(word) = count / size(doc1, 1);
-   
-   count = 2;
-   for i = 1:size(doc2, 1)
-      count = count + doc2(i,word); 
-   end
-   word_likelihood2(word) = count / size(doc2, 1);
-   
-end
-
-log_likelihood = abs(log(word_likelihood1) - log(word_likelihood2));
+[log_doc1, log_doc2, log_likelihood] = naive_bayes_net(trainDataSparse, trainLabel);
 
 % list the N most discrimative word features
-% get the indices of the N largest elements in a matrix
 [sortedVals, sortedIdx] = sort(log_likelihood(:), 'descend');
 N = 10;
 maxIdxs = sortedIdx(1:N);
-disp(['Top ', num2str(N), ' words are:']);
-disp(words(maxIdxs))
+disp(['Top ', num2str(N), ' most discriminative words:']);
+for i = 1:length(maxIdxs)
+   disp([num2str(log_likelihood(maxIdxs(i))), words(maxIdxs(i))]);
+end
+
+% return the % of correctly classified articles
+% each document (represented by a row in sparse table), multiply
+% each word (a cell within a row) with the log probability in the doc
+
+% for the training set
+trainClassDoc1 = (trainDataSparse * log_doc1);
+trainClassDoc2 = (trainDataSparse * log_doc2);
+% then classify which document it is by whichever is larger
+trainClassification = (trainClassDoc1 < trainClassDoc2) + 1;
+trainCorrects = sum(trainClassification == trainLabel) / length(trainLabel);
+disp(['Classified ', num2str(trainCorrects * 100), '% correct for training data.']);
+
+% for the testing set
+[test_log_doc1, test_log_doc2, ~] = naive_bayes_net(trainDataSparse, trainLabel);
+testClassDoc1 = (testDataSparse * test_log_doc1);
+testClassDoc2 = (testDataSparse * test_log_doc2);
+% then classify which document it is by whichever is larger
+testClassification = (testClassDoc1 < testClassDoc2) + 1;
+testCorrects = sum(testClassification == testLabel) / length(testLabel);
+disp(['Classified ', num2str(testCorrects * 100), '% correct for testing data.']);
+
---
+++ cs486/a3/naive_bayes_net.m
@@ -0,0 +1,38 @@
+function [log_doc1, log_doc2, log_likelihood] = naive_bayes_net(sparse_data, label)
+
+% split training set into two documents
+doc1 = [];
+doc2 = [];
+for i = 1:length(label)
+   if label(i) == 1
+       doc1 = [doc1; sparse_data(i,:)];
+   else
+       doc2 = [doc2; sparse_data(i,:)];
+   end
+end
+
+% calc Pr(word|label1) and Pr(word|label2)
+pr_word_doc1 = zeros(length(sparse_data(1,:)),1);
+pr_word_doc2 = zeros(length(sparse_data(1,:)),1);
+
+% start count at 1 for Laplace smoothing
+for word = 1:length(sparse_data(1,:))
+   count = 1;
+   for i = 1:size(doc1, 1)
+      count = count + doc1(i,word); 
+   end
+   pr_word_doc1(word) = count / size(doc1, 1);
+   
+   count = 1;
+   for i = 1:size(doc2, 1)
+      count = count + doc2(i,word); 
+   end
+   pr_word_doc2(word) = count / size(doc2, 1);
+   
+end
+
+log_doc1 = log(pr_word_doc1);
+log_doc2 = log(pr_word_doc2);
+log_likelihood = abs(log_doc1 - log_doc2);
+
+end
